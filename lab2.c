2d176b18f575a58a0a56badf1d4e524ee0bffa05
saliyvlad <saliy.vlad.jr@gmail.com>
2024-11-19 08:26:04 +0300

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -15,3 +15,37 @@ void main(){
 }
 	else printf("Год не является високосным\n");
 }
+#!/bin/bash
+
+# Проверка наличия аргумента
+if [ $# -ne 1 ]; then
+    echo "Ошибка: необходимо указать название каталога."
+    exit 1
+fi
+
+# Название каталога
+DIR_NAME=$1
+
+# Проверка наличия каталога на диске
+if [ ! -d "$DIR_NAME" ]; then
+    echo "Каталог '$DIR_NAME' не найден."
+    exit 1
+fi
+
+# Создание другого каталога для ссылок
+LINK_DIR="links"
+mkdir -p "$LINK_DIR"
+
+# Цикл для создания символных ссылок
+count=1
+for file in "$DIR_NAME"/*; do
+    # Проверка, чтобы создать не более 3 ссылок
+    if [ $count -le 3 ]; then
+        # Форматирование имени ссылки
+        link_name=$(printf "%s/%02d" "$LINK_DIR" "$count")
+        ln -s "$file" "$link_name"
+        count=$((count + 1))
+    else
+        break
+    fi
+done
